03511bfa6f03372928b66cb3d2d40aaa71e59ffb
fhrlzmn <fhrlzmn26@gmail.com>
2021-08-13 15:53:19 +0700

    Mengubah typo dan menambah class umur

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
-pubclic class main {
+public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Fahrul Zaman");
+
+        umur tUmur = new umur();
+        tUmur.setUmur(19);
+        System.out.println(tUmur.umur);
     }
 }
